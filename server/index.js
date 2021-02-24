a946e1201c26720414c57d214ffdd4d9d03bb996
Anthony Tomaro <atomaro@foundationsoft.com>
2021-02-24 09:49:15 -0500

    initial commit server

--- server/index.js
+++ server/index.js
@@ -1,15 +1,44 @@
 const express = require('express');
 const app = express();
 
+var LocalStorage = require('node-localstorage').LocalStorage;
+localStorage = new LocalStorage('./scratch');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-let todos = [{id: 1, name: 'Cohen Interview Assignment'}];
 
 const PORT = process.env.PORT || 3000
 
 app.get('/todos', (req, res) => {
-  return res.status(200).send(todos);
+  if (localStorage.todos) {
+    return res.status(200).send(JSON.parse(localStorage.todos));
+  }
+  else{
+    return res.status(200).send([]);
+  }
+});
+
+app.get('/tasks', (req, res) => {
+  if (localStorage.tasks) {
+    return res.status(200).send(JSON.parse(localStorage.tasks));
+  }
+  else{
+    return res.status(200).send([]);
+  }
+});
+
+app.get('/tasksfromtodotitle', (req, res) => {
+  return res.status(200).send(JSON.parse(localStorage.tasks.filter(i => i.title == todo)));
+});
+
+app.post('/todos', (req, res) => {
+  localStorage.todos = JSON.stringify(this.todos);
+  return res.status(200).send(localStorage.todos);
+});
+
+app.post('/tasks', (req, res) => {
+  localStorage.tasks = JSON.stringify(this.tasks);
+  return res.status(200).send(localStorage.tasks);
 });
 
 app.listen(PORT, () =>
